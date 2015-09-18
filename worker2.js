b5fe372fa8c79d6986b3b3f1984b764ce94feb06
Vikrant Varma <vikrant.varma94@gmail.com>
2015-09-18 05:53:28 +0530

    Use string_score instead of fuse

---
+++ worker2.js
@@ -0,0 +1,45 @@
+importScripts('string_score.min.js');
+
+var data;
+
+var fields = ['Department', 'Semester', 'Paper', 'Year'];
+
+onmessage = function(e) {
+    if (e.data.type === 'data') {
+        data = e.data.data;
+        for (var i = 0; i < data.length; ++i) {
+            for (var f = 0; f < fields.length; ++f) {
+                data[i][fields[f]] = data[i][fields[f]].toLowerCase();
+            }
+        }
+    } else if (e.data.type === 'query') {
+        var query = e.data.query;
+        query = query.toLowerCase();
+        var terms = query.split(/\s+/);
+        var fuzz = 0.3;
+        console.log('got query: ', query);
+        for (var j = 0; j < data.length; ++j) {
+            data[j].score = {'Department': 0, 'Semester': 0, 'Paper': 0, 'Year': 0, 'total': 1};
+            for (var t = 0; t < terms.length; ++t) {
+                var maxTermScore = 0, maxField;
+                for (var f = 0; f < fields.length; ++f) {
+                    var termFieldScore = data[j][fields[f]].score(terms[t], fuzz);
+                    if (termFieldScore > maxTermScore) {
+                        maxTermScore = termFieldScore;
+                        maxField = f;
+                    }
+                }
+                data[j].score[fields[maxField]] = maxTermScore;
+            }
+            for (var f = 0; f < fields.length; ++f) {
+                data[j].score.total += Math.max(data[j].score[fields[f]], 0.2) - 0.2;
+            }
+        }
+        data.sort(function(a, b) {
+            return b.score.total - a.score.total;
+        });
+        var results = data.slice(0, 20);
+        console.log('results: ', results);
+        postMessage(results);
+    }
+};
