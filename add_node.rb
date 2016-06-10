73935f68d239f09d6cab596b8718d0b6d4be5526
Athitya Kumar <athityakumar@gmail.com>
2016-06-10 22:38:59 +0530

    Added batch-insert feature. Updated auto-year & auto-semester feature.

--- add_node.rb
+++ add_node.rb
@@ -10,9 +10,37 @@ end
 
 fileData = File.read("data.json")
 obj = JSON.parse(fileData)
-last_sem = obj.last["Semester"]
+
+# Initialise time periods.Dates have the format of yyyy-mm-dd
+this_year = Date.today.strftime("%Y")
+MID_SPRING_PERIOD = (this_year+"-02-15 to "+this_year+"-04-30").split(" to ")
+END_SPRING_PERIOD = (this_year+"-05-01 to "+this_year+"-09-30").split(" to ")
+MID_AUTUMN_PERIOD = (this_year+"-10-01 to "+this_year+"-11-30").split(" to ")
+END_AUTUMN_PERIOD = (this_year+"-12-01 to "+(this_year.to_i+1).to_s+"-02-14").split(" to ")
 
 puts "#{obj.length} objects earlier!"
+
+# Just make sure that batch is not "" as while loop will never execute
+batch = "h"
+batch_semester = ""
+batch_year = ""
+today = Date.today.to_s
+
+while !["y","Y","n","N"].include? batch
+	puts "Do you want to batch insert year & semester for the papers? [y/Y for Yes, n/N for No) "
+	batch = gets.chomp
+	if batch.downcase == "y"
+		puts "Enter batch semester : "
+		batch_semester = gets.chomp
+		puts "Enter batch year : "
+		batch_year = gets.chomp
+	elsif batch.downcase == "n"
+		puts "Batch insert is not chosen."
+	else
+		puts "Invalid choice chosen - Use only y/Y/n/N"
+		batch = "h"
+	end
+end
 
 while true
 	puts "Enter yet another paper? (press enter to continue, N/n to exit and write to file)"
@@ -27,8 +55,12 @@ while true
 	puts "Enter department: "
 	department = gets.chomp
 
-	puts "Enter semester (mid spring 2016): "
-	semester = gets.chomp
+	if batch_semester.empty?
+		puts "Enter semester (mid spring 2016): "
+		semester = gets.chomp
+	else
+		semester = batch_semester
+	end
 
 	puts "Enter name of the paper (Basic Electronics): "
 	paper = gets.chomp
@@ -36,25 +68,29 @@ while true
 	puts "Enter link to the paper: "
 	link = gets.chomp
 
-	puts "Enter the year that this paper belongs to (2016): "
-	year = gets.chomp
+	if batch_year.empty?
+		puts "Enter the year that this paper belongs to (2016): "
+		year = gets.chomp
+	else
+		year = batch_year
+	end	
 
 	if year.length == 0
-		year = Date.today.strftime("%Y")
+		year = this_year
 	end
 
 	if semester.length == 0
-		semester = "" 
-		if last_sem.include? "mid"
-			semester = last_sem.gsub("mid","end") 
+		if today >= MID_SPRING_PERIOD[0] && today <= MID_SPRING_PERIOD[1]
+			semester = "mid spring "+this_year
+		elsif today >= END_SPRING_PERIOD[0] && today <= END_SPRING_PERIOD[1]
+			semester = "end spring "+this_year
+		elsif today >= MID_AUTUMN_PERIOD[0] && today <= MID_AUTUMN_PERIOD[1]
+			semester = "mid autumn "+this_year
+		elsif today >= END_AUTUMN_PERIOD[0] && today <= END_AUTUMN_PERIOD[1]
+			semester = "end autumn "+this_year
 		else
-			semester = last_sem.gsub("end","mid")
-			if last_sem.include? "spring"
-				semester = semester.gsub("spring","autumn")
-			else
-				semester = semester.gsub("autumn","spring")
-				semester = semester.gsub(last_sem.split(" ")[2],((last_sem.split(" ")[2].to_i)+1).to_s)
-			end
+			puts "Time period initialisation seems wrong."
+			break	 
 		end
 	end
 
